8a19285e21a282a07e96e1703f1c2282fc7a7252
Harley Rayner <harleymteden@gmail.com>
2018-09-14 20:18:35 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,9 +18,9 @@ class Triangle
   end
 
   # def valid?
-    @length1 + @length2 > @length3
-    @length1 + @length3 > @length2
-    @length2 + @length3 > @length1
+    # @length1 + @length2 > @length3
+    # @length1 + @length3 > @length2
+    # @length2 + @length3 > @length1
   #   @length1 == 0 || @length2 == 0 || @length3 == 0
   # end
 
