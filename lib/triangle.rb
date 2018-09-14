28231354f112c41bcc99fff71c2cfc4afb68b4a3
Harley Rayner <harleymteden@gmail.com>
2018-09-14 20:10:09 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -38,17 +38,17 @@ class Triangle
         raise TriangleError
       end
     end
-  end 
+  end
       #raise error
 
 
 
 
-      @length1 + @length2 > @length3 || @length1 + @length3 > @length2 || @length2 + @length3 > @length1 || @length1 == 0 || @length2 == 0 || @length3 == 0
-      begin
-
-    end
-
-  end
+  #     @length1 + @length2 > @length3 || @length1 + @length3 > @length2 || @length2 + @length3 > @length1 || @length1 == 0 || @length2 == 0 || @length3 == 0
+  #     begin
+  #
+  #   end
+  #
+  # end
 
 end
