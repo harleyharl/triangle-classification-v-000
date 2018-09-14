544a517fa1936207e45e98f190484ffd92c83dcd
Harley Rayner <harleymteden@gmail.com>
2018-09-14 20:23:33 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,40 +11,31 @@ class Triangle
   end
 
   def initialize(length1, length2, length3)
-    # Triangle.new(length1, length2, length3)
     @length1 = length1
     @length2 = length2
     @length3 = length3
   end
 
-  # def valid?
-    # @length1 + @length2 > @length3
-    # @length1 + @length3 > @length2
-    # @length2 + @length3 > @length1
-  #   @length1 == 0 || @length2 == 0 || @length3 == 0
-  # end
+  def valid?
+    @length1 + @length2 > @length3
+    @length1 + @length3 > @length2
+    @length2 + @length3 > @length1
+    @length1 > 0 && @length2 > 0 && @length3 > 0
+  end
 
   def kind
-    if @length1 == @length2 && @length1 == @length3
-      :equilateral
-    elsif
-      @length1 == @length2 && @length1 != @length3 || @length2 == @length3 && @length2 != @length1 || @length3 == @length1 && @length3 != @length2
-      :isosceles
-    elsif
-      @length1 != @length3 && @length3 != @length2
-      :scalene
-    elsif
-      @length1 <= 0 || @length2 <= 0 || @length3 <= 0
-      begin
-          raise TriangleError
-      end
-    elsif
-      # "triangle inequality checker"
-      @length1 + @length2 > @length3 || @length1 + @length3 > @length2 || @length2 + @length3 > @length1
-      begin
-          raise TriangleError
+    if self.valid?
+        if @length1 == @length2 && @length1 == @length3
+          :equilateral
+        elsif
+          @length1 == @length2 && @length1 != @length3 || @length2 == @length3 && @length2 != @length1 || @length3 == @length1 && @length3 != @length2
+          :isosceles
+        elsif
+          @length1 != @length3 && @length3 != @length2
+          :scalene
         end
-    end
+      else
+        TriangleError
   end
       #raise error
 
